385a408f887572237c9980d1cb87b11d06adb3aa
Sulav Parajuli <sulavparajuli82@gmail.com>
2024-08-20 23:13:06 +0545

    project detail page

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -14,6 +14,9 @@ const ProjectDetail = () => {
     return <Error />;
   }
 
+  // Check if the tag[1] link is a video file
+  const isVideo = project.tags[1].link.match(/\.(mp4|webm|ogg)$/i);
+
   return (
     <div className="container acontainer pb-5">
       <div className="container-fluid reveal ">
@@ -29,19 +32,36 @@ const ProjectDetail = () => {
             <h5>Technologies Used:</h5>
             <ul>{project.technologies}</ul>
             <div className="mt-3 buttons pb-3">
-              <a
-                href={project.tags[1].link}
-                className="main-button"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {project.tags[1].text}
-              </a>
+              {isVideo ? (
+                <video
+                  controls
+                  style={{ width: "100%" }}
+                  src={new URL(`${project.tags[1].link}`, import.meta.url).href}
+                  alt={project.title}
+                >
+                  Your browser does not support the video tag.
+                </video>
+              ) : (
+                <a
+                  href={project.tags[1].link}
+                  className="main-button"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    display: project.tags[1].link === "" ? "none" : "block"
+                  }}
+                >
+                  {project.tags[1].text}
+                </a>
+              )}
               <a
                 href={project.tags[2].link}
                 className="sub-button"
                 target="_blank"
                 rel="noopener noreferrer"
+                style={{
+                  display: project.tags[2].link === "" ? "none" : "block"
+                }}
               >
                 {project.tags[2].text}
               </a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,357 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-//import files
-import rajendrakhatri from "../assets/images/project/rajendrakhatri.png";
-import EthTix from "../assets/images/project/ethtix.png";
-import bibek from "../assets/images/project/bibek.png";
-import swopnilstore from "../assets/images/project/swopnilstore.png";
-import swgraphic from "../assets/images/project/swopnilstoregraphics.png";
-import simon from "../assets/images/project/simon.png";
-import macithub from "../assets/images/project/macithub.png";
-import jazzyventure from "../assets/images/project/jazzyventure.png";
-import drumkit from "../assets/images/project/drum-kit.png";
-import graphicstimeline from "../assets/images/project/graphicstimeline.png";
+import projectsData from "../data/projects.json";
 //import components
 import ProjectDetail from "./ProjectDetail";
 
 const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
-  const projectItems = [
-    {
-      id: "a",
-      filter: "filter-web",
-      link: "https://github.com/sulav-parajuli/EthTix",
-      image: EthTix,
-      alt: "EthTix",
-      title: "Final Year Project- EthTix",
-      subtitle: "Decentralized Ticketing Application",
-      previewText:
-        "This is a Decentralized Ticketing Application that my team and I created as our final year project. It is a blockchain-based application that enables users to buy and sell tickets using Sepolia Test Ethereum cryptocurrency.",
-      technologies:
-        "Solidity, Ether.js, React.js, Node.js, Hardhat Framework, Sepolia Test Eth, and more.",
-      tags: [
-        {
-          link: "../assets/images/project/ethtix.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "https://github.com/sulav-parajuli/EthTix",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "b",
-      filter: "filter-web",
-      link: "https://rajendrakhatri.com.np",
-      image: rajendrakhatri,
-      alt: "Portfolio Website",
-      title: "Portfolio Website",
-      subtitle: "Rajendra Khatri",
-      previewText:
-        "This is a simple portfolio website for Rajendra Khatri, a Business Man. The website showcases his work and provides information about his services.",
-      technologies: "HTML, CSS, JavaScript, JQuery and Bootstrap",
-      tags: [
-        {
-          link: "../assets/images/project/rajendrakhatri.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "https://rajendrakhatri.com.np",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "c",
-      filter: "filter-web",
-      link: "https://sulav-parajuli.github.io/Bibek/",
-      image: bibek,
-      alt: "Portfolio Website",
-      title: "Portfolio Website",
-      subtitle: "Bibek Bajagain",
-      previewText:
-        "This is a simple portfolio website for Bibek Bajagain, a Genius Developer. The website showcases his work and provides information about his services.",
-      technologies: "HTML, CSS, JavaScript, JQuery and Bootstrap",
-      tags: [
-        {
-          link: "../assets/images/project/bibek.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "https://sulav-parajuli.github.io/Bibek/",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "https://github.com/sulav-parajuli/Bibek/",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "d",
-      filter: "filter-web",
-      link: "https://jazzyventure.com.np/",
-      image: jazzyventure,
-      alt: "Static Private Company Website",
-      title: "Company Website",
-      subtitle: "Jazzy Venture",
-      previewText: `This website is a static site providing basic information about the company "Jazzy Venture Pvt. Ltd.", including its locations and contact details.`,
-      technologies: "HTML, CSS, JavaScript, JQuery and Bootstrap",
-      tags: [
-        {
-          link: "../assets/images/portfolio/jazzyventure.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "https://jazzyventure.com.np/",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "https://github.com/sulav-parajuli/jazzyventure",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "e",
-      filter: "filter-web",
-      link: "https://swopnilstore.com.np/",
-      image: swopnilstore,
-      alt: "Grocery Website",
-      title: "Simple Grocery Website",
-      subtitle: "Swopnil Store",
-      technologies: "HTML, CSS, JavaScript, JQuery and Bootstrap",
-      previewText: `This is a simple static grocery website providing basic
-              information about the grocery store, including its locations and
-              contact details. The website is integrated with SEO through Google Search Console. Additionally, I have set up a
-            Google Business profile
-          for this business.`,
-      tags: [
-        {
-          link: "../assets/images/portfolio/swopnilstore.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "https://swopnilstore.com.np/",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "https://github.com/sulav-parajuli/swopnilstore",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "j",
-      filter: "filter-web",
-      link: "https://github.com/sulav-parajuli/Swopnil-Store_new",
-      image: swopnilstore,
-      alt: "Grocery Application",
-      title: "Grocery Application",
-      subtitle: "Swopnil Store",
-      technologies:
-        "HTML, CSS, JavaScript, Blade, Bootstrap, Laravel, Google recaptcha, MySql, Xampp, Apache etc.",
-      previewText: `This grocery web application provides comprehensive information about the store, from essential details to dynamic updates. Users can explore various features, including store locations, a complete list of grocery items, their ledgers, and contact details. Designed for user convenience, it ensures a seamless shopping experience with intuitive navigation and up-to-date information, making grocery shopping more efficient and enjoyable. The application is currently in development, is not used externally, and does not have live access yet. `,
-      tags: [
-        {
-          link: "../assets/images/portfolio/swopnilstore.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "https://github.com/sulav-parajuli/Swopnil-Store_new",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    }
-    // Add more project items here
-  ];
+  const [selectedProjects, setSelectedProjects] = useState([]);
+  const [otherProjects, setOtherProjects] = useState([]);
 
-  const otherItems = [
-    {
-      id: "f",
-      filter: "filter-games",
-      link: "https://sulav-parajuli.github.io/Drum-kit/",
-      image: drumkit,
-      alt: "Drum Kit",
-      title: "Drum Kit",
-      subtitle: "Drum Game",
-      technologies: "HTML, CSS, JavaScript, JQuery and Bootstrap",
-      previewText: `This is a simple Drum Kit game that I created while learning
-              JavaScript. It is a fun and interactive game that allows users to
-              play the drums using their keyboard. I was inspired to create this
-              game after taking a course on Udemy instructed by Angela Yu.`,
-      tags: [
-        {
-          link: "../assets/images/portfolio/drum-kit.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "https://sulav-parajuli.github.io/Drum-kit/",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "https://github.com/sulav-parajuli/Drum-kit",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "g",
-      filter: "filter-games",
-      link: "https://sulav-parajuli.github.io/Simon-Game/",
-      image: simon,
-      alt: "Simon Game",
-      title: "Simon Game",
-      subtitle: "Repeated Sequence Mind Game",
-      technologies: "HTML, CSS, JavaScript, JQuery and Bootstrap",
-      previewText: `This is a simple Simon Game that I created while learning
-              JavaScript. It is a fun and interactive game that allows users to
-              test their memory by repeating a sequence of colors. I was
-              inspired to create this game after taking a course on Udemy
-              instructed by Angela Yu.`,
-      tags: [
-        {
-          link: "../assets/images/portfolio/simon.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "https://sulav-parajuli.github.io/Simon-Game/",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "https://github.com/sulav-parajuli/Simon-Game",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "h",
-      filter: "filter-design",
-      link: "https://www.facebook.com/mac.it.club",
-      image: macithub,
-      alt: "Graphics Designing",
-      title: "Graphic Designing",
-      subtitle: "Macithub",
-      technologies: "Adobe Photoshop, Adobe Illustrator etc.",
-      previewText: `These are simple social media graphic designs that I created for
-              the Macithub Student Club at my college. The designs showcase the
-              club's logo and provide information about the club's activities
-              and events.`,
-      tags: [
-        {
-          link: "../assets/images/portfolio/macithub.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "https://www.facebook.com/mac.it.club",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "i",
-      filter: "filter-design",
-      link: "https://www.facebook.com/swopnilstore",
-      image: swgraphic,
-      alt: "Graphics Designing",
-      title: "Graphic Designing",
-      subtitle: "Swopnil Store",
-      technologies: "Adobe Photoshop, Adobe Illustrator etc.",
-      previewText: `These are simple social media graphic designs that I created for
-              Swopnil Store, a grocery store in Pokhara. The designs showcase
-              the store's logo and provide information about the store's
-              products and services.`,
-      tags: [
-        {
-          link: "../assets/images/portfolio/swopnilstoregraphics.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "https://www.facebook.com/swopnilstore",
-          icon: "fas fa-external-link-alt",
-          text: "Visit Site"
-        },
-        {
-          link: "",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
-    },
-    {
-      id: "k",
-      filter: "filter-design",
-      link: "../assets/images/portfolio/graphics timeline.mp4",
-      image: graphicstimeline,
-      alt: "Graphics Designing",
-      title: "Graphic Timeline Design Art",
-      subtitle: "Sketch Timeline",
-      technologies: "Adobe Photoshop, Adobe Illustrator etc.",
-      previewText: `This graphic timeline artwork, created using Adobe Photoshop, beautifully illustrates the evolution of my photo through various stages of artistic transformation. Each frame captures a distinct moment in the creative process, showcasing the transition from a plain white artboard to the final piece.`,
-      tags: [
-        {
-          link: "../assets/images/portfolio/graphicstimeline.png",
-          icon: "fas fa-plus",
-          text: "View"
-        },
-        {
-          link: "../assets/images/portfolio/graphics timeline.mp4",
-          icon: "fas fa-external-link-alt",
-          text: "View Art Work"
-        },
-        {
-          link: "",
-          icon: "fab fa-github",
-          text: "Github"
-        }
-      ]
+  const getImagePath = (imageName) => {
+    try {
+      return new URL(`${imageName}`, import.meta.url).href;
+    } catch (error) {
+      console.error("Error loading image:", error);
+      return null;
     }
-  ];
+  };
+
+  useEffect(() => {
+    // Define the IDs for the selected projects
+    const selectedProjectIds = ["a", "b", "c", "d", "e", "j"];
+
+    // Filter projects based on whether their ID is in the selectedProjectIds array
+    const selected = projectsData.filter((project) =>
+      selectedProjectIds.includes(project.id)
+    );
+    const other = projectsData.filter(
+      (project) => !selectedProjectIds.includes(project.id)
+    );
+
+    setSelectedProjects(selected);
+    setOtherProjects(other);
+  }, []);
 
   return (
     <>
@@ -364,22 +45,22 @@ const Projects = () => {
             See my works
           </h1>
 
-          {projectItems.map((item) => (
+          {selectedProjects.map((item) => (
             <article
               key={item.id}
               className={`postcard blue text-black project-item ${item.filter}`}
               data-filter={`.${item.filter}`}
             >
-              <a className="postcard__img_link" href={item.link}>
+              <a className="postcard__img_link" href={item.tags[1].link}>
                 <img
                   className="postcard__img"
-                  src={item.image}
+                  src={getImagePath(item.tags[0].link)}
                   alt={item.alt}
                 />
               </a>
               <div className="postcard__text">
                 <h1 className="postcard__title blue">
-                  <a href={item.link}>{item.title}</a>
+                  <a href={item.tags[1].link}>{item.title}</a>
                 </h1>
                 <div className="postcard__subtitle large">
                   <p>{item.subtitle}</p>
@@ -399,13 +80,27 @@ const Projects = () => {
                       <i className="fas fa-plus"></i>&nbsp;{item.tags[0].text}
                     </Link>
                   </li>
-                  <li className="tag__item">
+                  <li
+                    className="tag__item"
+                    style={{
+                      display:
+                        item.tags[1].link.match(/\.(mp4|webm|ogg)$/i) ||
+                        item.tags[1].link === ""
+                          ? "none"
+                          : "block"
+                    }}
+                  >
                     <a href={item.tags[1].link}>
                       <i className="fas fa-external-link-alt"></i>&nbsp;
                       {item.tags[1].text}
                     </a>
                   </li>
-                  <li className="tag__item">
+                  <li
+                    className="tag__item"
+                    style={{
+                      display: item.tags[2].link === "" ? "none" : "block"
+                    }}
+                  >
                     <a href={item.tags[2].link}>
                       <i className="fab fa-github"></i>&nbsp;{item.tags[2].text}
                     </a>
@@ -426,22 +121,22 @@ const Projects = () => {
             Other Works
           </h1>
 
-          {otherItems.map((item) => (
+          {otherProjects.map((item) => (
             <article
               key={item.id}
               className={`postcard blue text-black project-item ${item.filter}`}
               data-filter={`.${item.filter}`}
             >
-              <a className="postcard__img_link" href={item.link}>
+              <a className="postcard__img_link" href={item.tags[1].link}>
                 <img
                   className="postcard__img"
-                  src={item.image}
+                  src={getImagePath(item.tags[0].link)}
                   alt={item.alt}
                 />
               </a>
               <div className="postcard__text">
                 <h1 className="postcard__title blue">
-                  <a href={item.link}>{item.title}</a>
+                  <a href={item.tags[1].link}>{item.title}</a>
                 </h1>
                 <div className="postcard__subtitle large">
                   <p>{item.subtitle}</p>
@@ -470,13 +165,31 @@ const Projects = () => {
                       <i className="fas fa-plus"></i>&nbsp;{item.tags[0].text}
                     </Link>
                   </li>
-                  <li className="tag__item">
-                    <a href={item.tags[1].link}>
+                  <li
+                    className="tag__item"
+                    style={{
+                      display:
+                        item.tags[1].link.match(/\.(mp4|webm|ogg)$/i) ||
+                        item.tags[1].link === ""
+                          ? "none"
+                          : "block"
+                    }}
+                  >
+                    <a
+                      href={item.tags[1].link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <i className="fas fa-external-link-alt"></i>&nbsp;
                       {item.tags[1].text}
                     </a>
                   </li>
-                  <li className="tag__item">
+                  <li
+                    className="tag__item"
+                    style={{
+                      display: item.tags[2].link === "" ? "none" : "block"
+                    }}
+                  >
                     <a href={item.tags[2].link}>
                       <i className="fab fa-github"></i>&nbsp;{item.tags[2].text}
                     </a>
